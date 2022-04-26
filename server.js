76aea2dc8f43a196c1e601a0e8707181862078cf
Joseph <jgwang@ad.unc.edu>
2022-04-26 13:09:12 -0400

    primary colors

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ if (args.log == true) {
   app.use(morgan('combined', { stream: WRITESTREAM }));
 } 
 
-// Creates sqllite database jason john laaaammmmeeejacobspoemailstify psubscribe and likeremium can you mcqueerip gravestonen only if you canada firstmy lighnint   what what rick kachoww aspeeeebuzz buzzdnd morty what   ...ffjriposephffsffffdfbig greenfffds    leadelike to climbrboard high score
+// Creates sqllite database jason john is this green enough laaaammmmeeejacobspoemailstify psubscribe and likeremium can you mcqueerip gravestonen only if you canada firstmy lighnint   what what rick kachoww aspeeeebuzz buzzdnd morty what   ...ffjriposephffsffffdfbig greenfffds    leadelike to climbrboard high score
 app.use((req, res, next) => {
   let logdata = {
     remoteaddr: req.ip,
