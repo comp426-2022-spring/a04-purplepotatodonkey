a7666a65c8602fdb277e45feff2652a3945aacf6
Joseph <jgwang@ad.unc.edu>
2022-04-26 13:00:49 -0400

    user

--- database.js
+++ database.js
@@ -4,7 +4,7 @@
 const Database = require('better-sqlite3');
 
 // Connect to a database or create one if it doesn't exist yet.
-const db = new Database('user.db');
+const db = new Database('log.db');
 
 // Is the database initialized or do we need to initialize it?
 const stmt = db.prepare(`
